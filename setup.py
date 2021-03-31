791289edaf8273392020f237fa2433c2c019730a
Ira Horecka <ira89@icloud.com>
2021-03-30 22:15:23 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,30 +1,9 @@
-import pycraigslist
-from setuptools import setup, find_packages
+"""
+PEP 517 doesn’t support editable installs
+so this file is currently here to support "pip install -e ."
+"""
+from setuptools import setup
 
-
-_classifiers = [
-    "Programming Language :: Python :: 3.5",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: 3.9",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: MIT License",
-    "Topic :: Software Development :: Libraries",
-    "Topic :: Utilities",
-]
-
-if __name__ == "__main__":
-    setup(
-        name="pycraigslist",
-        version=pycraigslist.__version__,
-        author="Ira Horecka",
-        author_email="ira89@icloud.com",
-        url="https://github.com/irahorecka/pycraigslist",
-        py_modules=["pycraigslist"],
-        description="A simple and expressive Craigslist API wrapper.",
-        long_description=open("README.rst", encoding="utf-8").read(),
-        license="MIT",
-        classifiers=_classifiers,
-        packages=find_packages(),
-    )
+setup(
+    use_scm_version={"write_to": "pycraigslist/_version.py"}, setup_requires=["setuptools_scm"],
+)
