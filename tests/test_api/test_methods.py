1dde3264ba5262161dcf751144a9630861878104
Ira Horecka <ira89@icloud.com>
2021-07-15 01:11:16 -0700

    add test - validate suggested subclasses

--- tests/test_api/test_methods.py
+++ tests/test_api/test_methods.py
@@ -8,6 +8,7 @@ Tests non-querying pycraigslist attributes, instance, and class methods.
 from pytest import mark
 
 import specs
+import pycraigslist
 
 
 @mark.parametrize("parent", specs.obj.pycraigslist_parents)
@@ -23,6 +24,17 @@ def test_dtype_get_categories(parent):
     """Tests class method `get_categories()` returns dict object,
     i.e. a reference for its subclass modules."""
     assert isinstance(parent.get_categories(), dict)
+
+
+@mark.parametrize("parent", specs.obj.pycraigslist_parents)
+def test_valid_sub_classes_from_categories(parent):
+    """Tests every subclass method suggested in `get_categories()` is a valid object."""
+    parent_class_str = str(parent).split("'")[1]
+    for subclass_key in parent.get_categories():
+        # Throws AttributeError (i.e. fails test) if subclass isn't defined in pycraigslist/api.py
+        eval(f"{parent_class_str}.{subclass_key}")
+    # Test passes if every suggested subclass of a parent class is valid
+    assert True
 
 
 @mark.parametrize("parent", specs.obj.pycraigslist_parents)
